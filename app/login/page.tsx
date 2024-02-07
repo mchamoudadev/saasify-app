256c8d8a3a59bdc843b373444925a73d31d0f286
mchamoudadev <mchamuuda@gmail.com>
2024-02-07 12:00:01 +0300

    get user data from the client

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,12 +11,23 @@ import {
 } from "@/components/ui/card"
 import { Button } from '@/components/ui/button'
 import { FcGoogle } from 'react-icons/fc'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 
 
 const LoginPage = () => {
+
+
+    const session = useSession()
+
+    console.log(session)
+
+    // if (!session.data) return <h1>loading...</h1>
+
+
     return (
         <div className='flex h-screen justify-center items-center'>
+            <h1>{session.data?.user?.name}</h1>
+            <img src={session.data?.user?.image} />
             <Card className='w-[350px]'>
                 <CardHeader>
                     <CardTitle>Login User</CardTitle>
