589e36f967e4f0686615291ec6ad08ef9fec5e00
mchamoudadev <mchamuuda@gmail.com>
2024-02-07 09:46:07 +0300

    shadcn crash course

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <h1>Hi</h1>
+      <Button variant={'outline'} size={'sm'}>Click Me</Button>
     </main>
   )
 }
