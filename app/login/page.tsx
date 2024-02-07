8afab59c651ed7be2649373037b36a4b3169c748
mchamoudadev <mchamuuda@gmail.com>
2024-02-07 10:01:18 +0300

    added login page

---
+++ app/login/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
+import { FcGoogle } from 'react-icons/fc'
+
+
+const LoginPage = () => {
+    return (
+        <div className='flex h-screen justify-center items-center'>
+            <Card className='w-[350px]'>
+                <CardHeader>
+                    <CardTitle>Login User</CardTitle>
+                    <CardDescription>Login with your google account.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className='grid w-full items-center gap-4'>
+                        <div className='flex flex-col space-y-1.5'>
+                            <Button variant={'outline'} className='flex space-x-2'>
+                                <FcGoogle className='w-6 h-6' />
+                                <span>Continue with google</span> </Button>
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default LoginPage
